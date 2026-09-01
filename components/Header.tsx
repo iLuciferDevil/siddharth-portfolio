9dc38d4990af97406ac81fac32f27fed84b12295
Siddharth Bhattacharjee <sidd.bhattacharjee@gmail.com>
2026-09-01 16:24:55 +0530

    Add Sovereign Brand book tab

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,14 +5,14 @@ export default function Header(){
     <header className="container nav">
       <Link href="/" className="logo">SIDDHARTH<span style={{fontWeight:400}}>.</span></Link>
       <nav className="navlinks">
-        <a href="/#work">Work</a><Link href="/services">Services</Link><Link href="/blog">Writing</Link><a href="/#about">About</a>
+        <a href="/#work">Work</a><Link href="/services">Services</Link><Link href="/blog">Writing</Link><Link href="/book">Book</Link><a href="/#about">About</a>
       </nav>
       <div className="nav-actions">
         <a className="pill" href="/#contact">Let&apos;s talk ↗</a>
         <details className="mobile-menu">
           <summary aria-label="Open navigation">Menu</summary>
           <div className="mobile-menu-panel">
-            <a href="/#work">Work</a><Link href="/services">Services</Link><Link href="/blog">Writing</Link><a href="/#about">About</a><a href="/#contact">Let&apos;s talk ↗</a>
+            <a href="/#work">Work</a><Link href="/services">Services</Link><Link href="/blog">Writing</Link><Link href="/book">Book</Link><a href="/#about">About</a><a href="/#contact">Let&apos;s talk ↗</a>
           </div>
         </details>
       </div>
