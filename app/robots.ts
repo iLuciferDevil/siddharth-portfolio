d218270f9fce0f6fbeb4d3e5188890578aed55d0
Siddharth Bhattacharjee <sidd.bhattacharjee@gmail.com>
2026-09-15 14:55:27 +0530

    Add robots policy and sitemap reference

---
+++ app/robots.ts
@@ -0,0 +1,3 @@
+import type {MetadataRoute} from 'next';
+
+export default function robots():MetadataRoute.Robots{return {rules:{userAgent:'*',allow:'/'},sitemap:'https://siddharthbhattacharjee.in/sitemap.xml',host:'https://siddharthbhattacharjee.in'};}
